08676a4aa79f18c9c974311464714c5a3e838a2b
giselle-fs <giselle.fs@gmail.com>
2021-10-05 16:56:39 -0300

    like button fixed

--- script.js
+++ script.js
@@ -10,3 +10,47 @@ menuToggle.addEventListener('click', () => {
 
   show = !show
 })
+
+//    Like Button
+var btnvar1 = document.getElementById('btnh1')
+
+function Toggle1() {
+  if (btnvar1.style.color == 'red') {
+    btnvar1.style.color = 'grey'
+  } else {
+    btnvar1.style.color = 'red'
+  }
+}
+
+//    Like Button 2
+var btnvar2 = document.getElementById('btnh2')
+
+function Toggle2() {
+  if (btnvar2.style.color == 'red') {
+    btnvar2.style.color = 'grey'
+  } else {
+    btnvar2.style.color = 'red'
+  }
+}
+
+//    Like Button 3
+var btnvar3 = document.getElementById('btnh3')
+
+function Toggle3() {
+  if (btnvar3.style.color == 'red') {
+    btnvar3.style.color = 'grey'
+  } else {
+    btnvar3.style.color = 'red'
+  }
+}
+
+//    Like Button 4
+var btnvar4 = document.getElementById('btnh4')
+
+function Toggle4() {
+  if (btnvar4.style.color == 'red') {
+    btnvar4.style.color = 'grey'
+  } else {
+    btnvar4.style.color = 'red'
+  }
+}
