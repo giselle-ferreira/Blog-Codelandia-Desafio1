78292e4d1d4d9bd990de29ee8da42521ab9efe27
giselle-fs <giselle.fs@gmail.com>
2021-10-05 10:59:01 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let show = true
+
+const menuSection = document.querySelector('.menu-section')
+const menuToggle = document.querySelector('.menu-toggle')
+
+menuToggle.addEventListener('click', () => {
+  document.body.style.overflow = show ? 'hidden' : 'initial'
+
+  menuSection.classList.toggle('on', show)
+
+  show = !show
+})
